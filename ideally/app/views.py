c8df933e1aa696ba00450d384014f85179ac8a59
Stefan Schneider <stefan.schneider@upb.de>
2020-07-05 19:23:43 +0200

    basic index

--- ideally/app/views.py
+++ ideally/app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+from django.views.generic.edit import FormView, CreateView, DeleteView, UpdateView
+from django.utils import timezone
+from django.template import loader
 
-# Create your views here.
+from .models import Idea
+
+
+class IndexView(generic.ListView):
+    template_name = 'app/index.html'
+    context_object_name = 'idea_list'
+
+    def get_queryset(self):
+        """Return all ideas"""
+        return Idea.objects.all().order_by('update_date')
--- ideally/project/settings.py
+++ ideally/project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ideally.app.apps.AppConfig',
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
