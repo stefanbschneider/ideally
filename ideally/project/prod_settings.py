65a97403c32f74ba03b44075962a018eb687e39a
Stefan Schneider <stefan.schneider@upb.de>
2020-07-19 18:15:02 +0200

    added production settings

---
+++ ideally/project/prod_settings.py
@@ -0,0 +1,13 @@
+""" Production Settings """
+from .settings import *
+
+
+############
+# SECURITY #
+############
+
+DEBUG = False
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+
+ALLOWED_HOSTS = ['ideally-app.herokuapp.com']
