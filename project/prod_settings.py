10b1f4a0b2b2a7e1e44b07e2f9f7333393ddb4b7
Stefan Schneider <stefan.schneider@upb.de>
2020-07-21 18:22:55 +0200

    turn on debugging

--- project/prod_settings.py
+++ project/prod_settings.py
@@ -8,14 +8,8 @@ from .settings import *
 # SECURITY #
 ############
 
-DEBUG = False
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db_prod.sqlite3'),
-#     }
-# }
+# TODO: turn off for real production
+DEBUG = True
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
