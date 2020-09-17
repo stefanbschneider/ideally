9c877898d2c22972b8b83f7f30f079c7529d02df
Stefan Schneider <stefan.schneider@upb.de>
2020-09-17 21:31:22 +0200

    display milestones for ideas

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Idea, Tag
+from .models import Idea, Tag, Milestone
 
 
 admin.site.register(Idea)
 admin.site.register(Tag)
+admin.site.register(Milestone)
